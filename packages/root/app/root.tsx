5bf559bc6b39cdeccef7b0d7847db72944841982
Jorge Galat <jrgglt@gmail.com>
2022-03-06 15:59:08 -0300

    updates

--- packages/root/app/root.tsx
+++ packages/root/app/root.tsx
@@ -1,3 +1,4 @@
+import type { MetaFunction, LinksFunction, ErrorBoundaryComponent } from "remix";
 import {
   Links,
   LiveReload,
@@ -5,21 +6,20 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  MetaFunction,
-  LinksFunction,
-} from 'remix';
+  Link,
+} from "remix";
 
-import styles from '~/styles/global.css';
+import styles from "~/styles/global.css";
 
 export const meta: MetaFunction = () => {
   return {
-    title: 'jg.ar',
+    title: "jg.ar",
     description: `Jorge Galat - I'm a full stack developer based in Rosario, Argentina`,
   };
 };
 
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: styles }];
+  return [{ rel: "stylesheet", href: styles }];
 };
 
 export default function App() {
@@ -40,3 +40,25 @@ export default function App() {
     </html>
   );
 }
+
+export const CatchBoundary: ErrorBoundaryComponent = ({ error }) => {
+  console.error(error);
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <main>
+          <h1>Not found :(</h1>
+          <p>
+            <Link to="/">Go back to home</Link>
+          </p>
+        </main>
+      </body>
+    </html>
+  );
+};
