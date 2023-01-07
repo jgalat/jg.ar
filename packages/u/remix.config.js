66c599ca9aa5f134db1752f911db7613adaeb7e2
Jorge Galat <jrgglt@gmail.com>
2023-01-07 13:01:57 -0300

    chore(packages): update dependencies

--- packages/u/remix.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-module.exports = {
-  serverBuildTarget: "cloudflare-workers",
-  server: "./server.js",
-  devServerBroadcastDelay: 1000,
-  ignoredRouteFiles: [".*"],
-};
