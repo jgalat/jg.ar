059b29e357b3f2760046abdc5d44bd2b97581fd3
Jorge Galat <jrgglt@gmail.com>
2023-06-24 11:16:50 -0300

    chore: upgrade root

--- packages/root/remix.config.js
+++ packages/root/remix.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ["**/.*"],
-  server: "./server.ts",
+  server: "./worker.ts",
   serverConditions: ["worker"],
   serverDependenciesToBundle: "all",
   serverMainFields: ["browser", "module", "main"],
