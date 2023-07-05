3942afaf5fb8657d454ba899496497190193c704
Jorge Galat <jrgglt@gmail.com>
2023-07-05 14:15:49 -0300

    fix: dark hover link color

--- packages/r/app/components/link.tsx
+++ packages/r/app/components/link.tsx
@@ -5,7 +5,7 @@ export default function Link({ className, ...props }: LinkProps) {
   return (
     <RemixLink
       className={clsx(
-        `underline decoration-dotted underline-offset-4 hover:text-pink-500 hover:decoration-solid focus:decoration-solid focus:text-pink-500 focus:decoration-pink-500 focus:outline-none`,
+        `underline decoration-dotted underline-offset-4 hover:text-pink-600 dark:hover:text-pink-500 hover:decoration-solid focus:decoration-solid focus:text-pink-500 focus:decoration-pink-500 focus:outline-none`,
         className
       )}
       {...props}
